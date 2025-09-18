bce6ea22613614f72addff8e9f71b5756b383df1
Dirk Petrautzki <dirk@dirk-petrautzki.de>
2025-09-18 07:59:27 +0200

    Use contains() for container membership checks

--- 2024/05/a.cpp
+++ 2024/05/a.cpp
@@ -17,7 +17,7 @@ int main() {
 
   unordered_map<int, unordered_set<int>> rules;
 
-  for (string line; getline(inputFile, line) && line.find('|') != string::npos;) {
+  for (string line; getline(inputFile, line) && line.contains('|');) {
     int from, to; 
     char delim; 
     stringstream(line) >> from >> delim >> to;
@@ -32,7 +32,7 @@ int main() {
 
     middleSum += [&rules, &ss, &seq, &pos]() {
       for (int n; ss >> n; ss.ignore(1, ',')) {
-        if (!all_of(rules[n].begin(), rules[n].end(), [&pos](int to) { return !pos.count(to); })) {
+        if (!all_of(rules[n].begin(), rules[n].end(), [&pos](int to) { return !pos.contains(to); })) {
           return 0;
         }
         seq.push_back(n);
