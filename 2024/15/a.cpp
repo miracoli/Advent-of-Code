6a190a2734bd2be8265d2185f098d1b2c835d9b5
Dirk Petrautzki <dirk@dirk-petrautzki.de>
2025-10-01 08:06:30 +0200

    Refactor day 10 and day 15 implementations

--- 2024/15/a.cpp
+++ 2024/15/a.cpp
@@ -7,14 +7,10 @@
 using namespace std;
 
 static const unordered_map<char, pair<int, int>> deltas = {{'<', {-1, 0}}, {'>', {1, 0}}, {'^', {0, -1}}, {'v', {0, 1}}};
-vector<string> grid;
-pair<int, int> robot;
-int gpsSum = 0;
-
-bool moveBox(const pair<int, int>& box, const pair<int, int>& delta) {
+bool moveBox(vector<string>& grid, int& gpsSum, const pair<int, int>& box, const pair<int, int>& delta) {
   if (auto newBox = pair<int, int>{box.first + delta.first, box.second + delta.second};
       grid[newBox.second][newBox.first] == '.' ||
-          (grid[newBox.second][newBox.first] == 'O' && moveBox(newBox, delta))) {
+          (grid[newBox.second][newBox.first] == 'O' && moveBox(grid, gpsSum, newBox, delta))) {
     gpsSum += 100 * delta.second + delta.first;
     grid[newBox.second][newBox.first] = 'O';
     return true;
@@ -28,6 +24,9 @@ int main() {
     cerr << "Error: input.txt not found" << endl;
     return 1;
   }
+  vector<string> grid;
+  pair<int, int> robot;
+  int gpsSum = 0;
   for (string line; getline(input, line) && !line.empty(); grid.emplace_back(std::move(line))) {
     for (int x = 0; x < line.size(); ++x) {
       if (line[x] == '@') {
@@ -42,7 +41,7 @@ int main() {
     for (char move : line) {
       auto [dx, dy] = deltas.at(move);
       pair<int, int> newRobot = {robot.first + dx, robot.second + dy};
-      if ((grid[newRobot.second][newRobot.first] == 'O' && moveBox(newRobot, {dx, dy})) || grid[newRobot.second][newRobot.first] == '.') {
+      if ((grid[newRobot.second][newRobot.first] == 'O' && moveBox(grid, gpsSum, newRobot, {dx, dy})) || grid[newRobot.second][newRobot.first] == '.') {
         grid[newRobot.second][newRobot.first] = '@';
         grid[robot.second][robot.first] = '.';
         robot = newRobot;
