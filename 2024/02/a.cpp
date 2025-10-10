2cd70bb19f523baabd273babff1f1d82db0e6ffa
Dirk Petrautzki <dirk@dirk-petrautzki.de>
2025-10-10 12:51:15 +0200

    Refactor 2024 Day 2 part A validation

--- 2024/02/a.cpp
+++ 2024/02/a.cpp
@@ -13,18 +13,25 @@ int main() {
     return 1;
   }
 
+  auto isValid = [](const vector<int>& nums) {
+    bool wrongSign = (nums[1] - nums[0]) > 0;
+    for (size_t i = 1; i < nums.size(); ++i) {
+      int diff = nums[i] - nums[i - 1];
+      if (abs(diff) < 1 || abs(diff) > 3 || signbit(diff) == wrongSign) {
+        return false;
+      }
+    }
+    return true;
+  };
+
   int validCount = 0;
 
   for (string line; getline(file, line);) {
     istringstream iss(line);
     vector<int> nums{ istream_iterator<int>(iss), {} };
-    bool wrongSign = !signbit(nums[1] - nums[0]);
-    bool isValid = true;
-    for (size_t i = 1; i < nums.size() && isValid; ++i) {
-      int diff = nums[i] - nums[i - 1];
-      isValid = !(abs(diff) < 1 || abs(diff) > 3 || signbit(diff) == wrongSign);
+    if (isValid(nums)) {
+      ++validCount;
     }
-    validCount += (int) isValid;
   }
 
   cout << validCount << endl;
