8d1bd06926be8ce4a78ac996853da6ab8b95cd5e
Dirk Petrautzki <dirk@dirk-petrautzki.de>
2025-10-23 12:39:45 +0200

    Update b.cpp

--- 2024/08/b.cpp
+++ 2024/08/b.cpp
@@ -22,13 +22,14 @@ int main() {
     int cols;
     set<pair<int, int>> antinodes;
 
-    for (string line; getline(inputFile, line); ++rows) {
+    for (string line; getline(inputFile, line); ) {
         cols = static_cast<int>(line.size());
         for (int x = 0; x < line.size(); ++x) {
             if (line[x] != '.') {
                 antennas[line[x]].emplace_back(x, rows);
             }
         }
+        ++rows;
     }
 
     for (const auto& [freq, positions] : antennas) {
