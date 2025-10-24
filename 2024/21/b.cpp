404ec5e51a1b2345025fdb2c9322237644f358d8
Dirk Petrautzki <dirk@dirk-petrautzki.de>
2025-10-24 07:07:08 +0200

    Update b.cpp

--- 2024/21/b.cpp
+++ 2024/21/b.cpp
@@ -11,12 +11,12 @@
 
 using namespace std;
 
-unordered_map<char, array<int8_t, 2>> positions = {{'7', {0, 0}}, {'8', {0, 1}}, {'9', {0, 2}},
+const unordered_map<char, array<int8_t, 2>> positions = {{'7', {0, 0}}, {'8', {0, 1}}, {'9', {0, 2}},
                                                    {'4', {1, 0}}, {'5', {1, 1}}, {'6', {1, 2}},
                                                    {'1', {2, 0}}, {'2', {2, 1}}, {'3', {2, 2}},
                                                    {'0', {3, 1}}, {'A', {3, 2}}, {'^', {0, 1}},
                                                    {'a', {0, 2}}, {'<', {1, 0}}, {'v', {1, 1}}, {'>', {1, 2}}};
-unordered_map<char, array<int8_t, 2>> directions = {{'^', {-1, 0}}, {'v', {1, 0}}, {'<', {0, -1}}, {'>', {0, 1}}};
+const unordered_map<char, array<int8_t, 2>> directions = {{'^', {-1, 0}}, {'v', {1, 0}}, {'<', {0, -1}}, {'>', {0, 1}}};
 unordered_map<string, uint64_t> cache;
 constexpr uint_fast8_t limit = 25;
 
